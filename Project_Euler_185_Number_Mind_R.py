98f46b04e0ff3d2b4bb939126cf5fd8a76f35109
José Fernando González Herrera <30704196+jfernandoghe@users.noreply.github.com>
2017-11-02 23:57:13 -0600

    Create Project_Euler_185_Number_Mind_R.py

---
+++ Project_Euler_185_Number_Mind_R.py
@@ -0,0 +1,99 @@
+import array
+import array
+num=int(input())
+nc={}
+nc2={}
+sq={}
+j, k, i, e, tot =  0, 0, 0, 0, 0
+sq2 = [[0 for x in range(12)] for y in range(num)] 
+sq3 = [[0 for x in range(12)] for y in range(num)]
+ot  = [[0 for x in range(12)] for y in range(10)]
+o = [0,0,0,0,0,0,0,0,0,0,0,0]
+for i in range (0,num):
+    entry = str(input()).split(" ")
+    sq[i] = str(entry[0])
+    nc[i] = str(entry[1])
+    nc2[i]= int(nc[i])
+while k==0:                             #Rellenar de 0 a 9 la matriz de salida
+    for i in range (0,10):
+        ot[i][j]=i
+        if i==9:
+            j=j+1
+        if j==12:
+            k=k+1
+j, k, i =  0, 0, 0
+#************************************************************************
+while k==0:                             #Rellenar SQ2 y SQ3
+    for i in range (0,num):               
+        sq3[i][j]=int(sq[i][j:j+1])
+        sq2[i][j]=int(sq[i][j:j+1])
+        if i==num-1:
+            j=j+1
+        if j==12:
+            k=k+1
+for i in range (0,num):                   #Eliminar los 0 de SQ3
+    if nc[i]=='0':
+        for j in range (0,12):
+            sq3[i][j]=0
+#************************************************************************
+while e==0:
+    for i in range (0,num):
+        for i in range (0,12):
+            if sq2[i][j]==0:
+                tot+=1
+    j, k, i =  0, 0, 0                      #Eliminar digitos cuando nc==0 de OT
+    for k in range (0,num):
+        if nc2[k]==0:
+            for i in range (0,10):
+                for j in range (0,12):
+                    if sq2[k][j]==ot[i][j]:
+                        ot[i][j]=0
+#************************************************************************ 
+    j, k, i =  0, 0, 0                      #Si hay 1 solo valor en ot se lo asigna a o
+    for i in range (0, 12):
+        for j in range (0,10):
+            if ot[j][i]!=0:
+                flag=ot[j][i]
+                k=k+1
+        if k==1:
+            o[i]=flag
+#************************************************************************ 
+    j, k, i, c =  0, 0, 0, 0                #Si 1 valor de los posibles OT aparece en SQ3
+    for i in range (0,num):
+        for j in range (0,12):
+            for k in range (0,10):
+                if ot[k][j]==sq2[i][j]:
+                    flag=ot[k][j]
+                    c=c+1
+            if c==1:
+                o[j]=flag
+#************************************************************************ 
+    j, k, i =  0, 0, 0                     #Si un digito de o es diferente de 0 y aparece en SQ3 disminuimos nc
+    for i in range (0,num):
+        for j in range (0,12):
+            if o[j]!=0 and o[j]==sq3[i][j]:
+                nc2[i]-=1
+#************************************************************************
+    j, k, i =  0, 0, 0                     #Si tenemos 1 digito en o hacemos toda la columna 0
+    for i in range (0,num):
+        for j in range (0,12):
+            if o[j]!=0:
+                sq3[i][j]=0
+#************************************************************************ 
+#for i in range (0,num):
+#    if nc2[i]==0:
+    if tot==num*12:
+        e+=1
+        print('Win!')
+#************************************************************************     
+print('************************************         RES')    
+print(o)
+print('************************************         OT')
+for i in range (0,10):                   #RES
+    print (ot[i])
+print('************************************         SQ3')
+for i in range (0,num):                   #SQ2
+    print (sq3[i],nc[i], nc2[i])
+print('************************************         SQ2')
+for i in range (0,num):                   #SQ3
+    print (sq2[i],nc[i],'   ', nc2[i])
